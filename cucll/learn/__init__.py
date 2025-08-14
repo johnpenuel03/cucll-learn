866876143190059bc17b52370693b8af9654ec44
John Penuel <jpenuel03@gmail.com>
2025-08-14 18:14:52 +0300

    fixed module export error

--- cucll/learn/__init__.py
+++ cucll/learn/__init__.py
@@ -1,6 +1,6 @@
 from .explore_data import explore_data
-from .clean import clean_data, handle_outliers, handle_missing, handle_duplicates
-from .utils import validate_dataframe
+from .clean_data import clean_data
+from .utils import validate_dataframe_input, handle_outliers, handle_missing, handle_duplicates
 
 __all__ = [
     'explore_data',
@@ -8,5 +8,5 @@ __all__ = [
     'handle_outliers',
     'handle_missing',
     'handle_duplicates',
-    'validate_dataframe'
+    'validate_dataframe_input'
 ]
